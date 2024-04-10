ed36e94edb8e6398fc0dc9b623c05668ad9a7e5d
Rayen <MR.RAYENK@GMAIL.COM>
2024-04-10 00:22:44 -0400

    Update firebase.tsx

--- firebase.tsx
+++ firebase.tsx
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth"; // Import the getAuth function for auth
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCs085LbQMw0tm_3yb3SMpDu5ILzo2pidw",
-  authDomain: "myocala-523d5.firebaseapp.com",
-  projectId: "myocala-523d5",
-  storageBucket: "myocala-523d5.appspot.com",
-  messagingSenderId: "227826453513",
-  appId: "1:227826453513:web:b4a6dacbfd5a7b075fbcd8",
-  measurementId: "G-91NNYYY4LX"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
